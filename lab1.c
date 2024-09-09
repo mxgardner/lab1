47a957eb09c61031c06b07efcdd6d1e90b1d1da7
Mariah <mariahgardner00@gmail.com>
2024-09-08 20:18:15 -0500

    Initalize data structures

--- lab1.c
+++ lab1.c
@@ -2,7 +2,40 @@
 // 1001576678
 // TODO: add command line used to compile code
 
+#include <stdio.h>
+
 int main void (){
+
+    int n;
+    
+    // Read the first input value, which is 'n'
+    scanf("%d", &n);
+
+    // Create arrays dynamically based on 'n'
+    int count[n];
+    for (int i = 0; i < n; i++) {
+        count[i] = 0;  // Initialize counters to 0
+    }
+
+    int index[n];
+    for (int i = 0; i < n; i++) {
+        index[i] = 0;
+    }
+
+    int map[n];
+    for (int i = 0; i < n; i++) {
+        map[i] = 0;
+    }
+
+    // Loop to read operations until termination (operation 0)
+    int operation;
+    while (scanf("%d", &operation) && operation != 0) {
+        if (operation == 1) {
+            // Add operations for values
+        } else if (operation == 2) {
+        }
+        // Continue conditions
+    }
 
     return 0;
 }
